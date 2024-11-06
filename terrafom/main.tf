79bb06deb56c3e31f284f762e4ebe209d11b392b
abhijeetshelar789 <abhijeetshelar789@gmail.com>
2024-11-06 10:20:22 +0530

    files addes

---
+++ terrafom/main.tf
@@ -0,0 +1,111 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "7512e392-13a7-4b3f-9e7f-eb323726d2d2"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "deploy"
+  location = "South India"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "deploy-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "sub" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "deploy-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"  # Static IP for persistent access
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"  # Adjust this to limit access to specific IPs
+    destination_address_prefix = "*"
+  }
+   security_rule {
+    name                       = "allow-http"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sub.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id  # Associate Public IP
+  }
+}
+
+# Associate the NSG with the NIC
+resource "azurerm_network_interface_security_group_association" "nic_nsg_association" {
+  network_interface_id     = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  depends_on = [azurerm_network_interface.nic]
+
+  name                = "deployprod"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "abhijeet"
+  admin_password      = "Password@890"  # Use a secure password
+
+  disable_password_authentication = false
+
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+output "public_ip_address" {
+  value       = azurerm_public_ip.public_ip.ip_address
+  description = "The public IP address of the virtual machine."
+}
